1092ca78bdfb375e699df7718a20e27ab23246e1
TakashiKAWAMOTO-TTS <atsugi-development@tbts.co.jp>
2020-07-28 09:41:14 +0900

    modify train.sh

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
-# python3 train.py 0 1
-# python3 train.py 1 1
-# python3 train.py 2 1
-# python3 train.py 3 1
-# python3 train.py 4 1
-# python3 train.py 5 1
-# python3 train.py 100 6
+python3 train.py 0 1
+python3 train.py 1 1
+python3 train.py 2 1
+python3 train.py 3 1
+python3 train.py 4 1
+python3 train.py 5 1
+python3 train.py 100 6
